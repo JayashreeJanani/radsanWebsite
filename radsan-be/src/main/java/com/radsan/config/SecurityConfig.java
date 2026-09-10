879181703ab16d3d3e9f63f7613d970563e3fa7d
Jayashree Janani <jayashree.199731@gmail.com>
2026-09-10 06:32:16 -0300

    Securitymodule completed

--- radsan-be/src/main/java/com/radsan/config/SecurityConfig.java
+++ radsan-be/src/main/java/com/radsan/config/SecurityConfig.java
@@ -2,30 +2,71 @@ package com.radsan.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.radsan.security.JwtAuthenticationFilter;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 public class SecurityConfig {
+	
+	 private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	 
+	 public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+	        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	    }
 	
 	@Bean
 	PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 	
-	 @Bean
-	    SecurityFilterChain securityFilterChain(HttpSecurity http)
-	            throws Exception {
 
-	        http
-	            .csrf(csrf -> csrf.disable())
-	            .authorizeHttpRequests(auth -> auth
-	                .anyRequest().permitAll()
-	            );
+	 
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http)
+	        throws Exception {
 
-	        return http.build();
-	    }
+	    http
+	        .csrf(csrf -> csrf.disable())
+
+	        .sessionManagement(session ->
+	            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	        )
+
+	        .exceptionHandling(exception -> exception
+	            .authenticationEntryPoint((request, response, authException) -> {
+	                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+	            })
+	        )
+
+	        .authorizeHttpRequests(auth -> auth
+	        		  .requestMatchers(
+	        			        HttpMethod.GET,
+	        			        "/api/products/**",
+	        			        "/api/faqs/**",
+	        			        "/api/blogs/**",
+	        			        "/api/testimonials/**",
+	        			        "/api/industries/**",
+	        			        "/api/categories/**",
+	        			        "/api/job_postings/**"
+	        			    ).permitAll()
+	            .anyRequest().authenticated()
+	        )
+
+	        .addFilterBefore(
+	            jwtAuthenticationFilter,
+	            UsernamePasswordAuthenticationFilter.class
+	        );
+
+	    return http.build();
+	}
 
 }
--- radsan-be/src/main/java/com/radsan/security/JwtService.java
+++ radsan-be/src/main/java/com/radsan/security/JwtService.java
@@ -8,6 +8,7 @@ import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -53,5 +54,30 @@ public class JwtService {
                 .signWith(getSigningKey())
                 .compact();
     }
+    
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+    
+    public boolean validateToken(String token) {
+
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+    
 
 }
